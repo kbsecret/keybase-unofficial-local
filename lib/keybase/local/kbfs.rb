d0abf1a582bf0ad4f496252771b362a8d650e726
William Woodruff <william@tuffbizz.com>
2017-10-24 19:39:35 -0400

    kbfs: Fill in Keybase::Local::KBFS
    Implements `KBFS.mounted?` and `KBFS.status`. `mounted?` tests whether
    or not KBFS is currently mounted, and `status` returns an
    `OpenStruct` mapping of the status file used to detect KBFS's state.
    
    See https://github.com/kbsecret/kbsecret/pull/42

--- lib/keybase/local/kbfs.rb
+++ lib/keybase/local/kbfs.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
 
+require "json"
+require "ostruct"
+
 module Keybase
   module Local
     # Represents an interface to KBFS.
-    # @note This is a stub.
     module KBFS
-      # stub
+      # The path to a hidden status file on KBFS.
+      # @note The presence of this file is used to determine whether or not the mountpoint
+      #  is actually KBFS or just a directory (since an accidental directory at the mountpoint
+      #  is unlikely to contain this file).
+      KBFS_STATUS_FILE = File.join Config::KBFS_MOUNT, ".kbfs_status"
+
+      class << self
+        # @return [Boolean] whether KBFS is currently mounted
+        def mounted?
+          File.exist? KBFS_STATUS_FILE
+        end
+
+        # @return [OpenStruct] a struct mapping of the contents of {KBFS_STATUS_FILE}
+        # @raise [Exceptions::KBFSNotRunningError] if KBFS is not mounted
+        def status
+          raise Exceptions::KBFSNotRunningError unless mounted?
+          JSON.parse File.read(KBFS_STATUS_FILE), object_class: OpenStruct
+        end
+      end
     end
   end
 end
