0040c62372bb87fffc8c20d395733745558e9359
William Woodruff <william@tuffbizz.com>
2017-10-24 23:41:41 -0400

    kbfs: Refine `mounted?` conditions
    `Keybase::Local::KBFS.mounted?` now tests whether both Keybase
    and KBFS are running before testing for a file on the mountpoint.
    
    This prevents `stat(2)` hangs whenever KBFS itself is running
    but Keybase isn't.
    
    See https://github.com/kbsecret/kbsecret/pull/42

--- lib/keybase/local/kbfs.rb
+++ lib/keybase/local/kbfs.rb
@@ -14,8 +14,28 @@ module Keybase
       KBFS_STATUS_FILE = File.join Config::KBFS_MOUNT, ".kbfs_status"
 
       class << self
-        # @return [Boolean] whether KBFS is currently mounted
+        # @return [Boolean] whether or not KBFS is currently running
+        # @note {running?} does *not* mean that KBFS is mounted. For that, see {mounted?}
+        def running?
+          if Gem.win_platform?
+            !`tasklist | find "kbfsfuse.exe"`.empty?
+          elsif RUBY_PLATFORM =~ /darwin/
+            !`pgrep kbfsfuse`.empty?
+          else
+            Dir["/proc/[0-9]*/comm"].any? do |comm|
+              File.read(comm).chomp == "kbfsfuse" rescue false
+            end
+          end
+        end
+
+        # @return [Boolean] whether or not KBFS is currently mounted
+        # @note The criteria for being mounted is as follows:
+        #  1. Keybase is running
+        #  2. KBFS is running
+        #  3. {KBFS_STATUS_FILE} exists
         def mounted?
+          return false unless Local.running?
+          return false unless running?
           File.exist? KBFS_STATUS_FILE
         end
 
