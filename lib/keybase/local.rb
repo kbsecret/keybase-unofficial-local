d2a8bbb31c429e19897048fe694ccf9419ab518c
William Woodruff <william@tuffbizz.com>
2017-10-25 23:04:46 -0400

    local: 0.2.1

--- lib/keybase/local.rb
+++ lib/keybase/local.rb
@@ -14,7 +14,7 @@ module Keybase
   # The namespace for `keybase-unofficial-local`.
   module Local
     # The current version of `keybase-unofficial-local`.
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
 
     extend Config
   end
